a8b8a76cb162872cab01efcf2b818d42271e8c68
Tonymans33 <tonymansour66@yahoo.com>
2021-12-29 16:41:32 +0200

    adding text watermark

--- watermark.py
+++ watermark.py
@@ -1,24 +1,26 @@
-
 import cv2 as cv
 import os
 import tkinter
 from tkinter import filedialog
 
-def browse_multiple_images():
 
+def browse_multiple_images():
     root = tkinter.Tk()
     root.withdraw()
 
-    images_paths = filedialog.askopenfilenames(initialdir='D:\Programming\College Study\IG\waterMarking\lib', title='Please select images')
+    images_paths = filedialog.askopenfilenames(initialdir='D:\Programming\College Study\IG\waterMarking\lib',
+                                               title='Please select images')
     return images_paths
 
-def browse_image():
 
+def browse_image():
     root = tkinter.Tk()
     root.withdraw()
 
-    images_path = filedialog.askopenfilename(initialdir='D:\Programming\College Study\IG\waterMarking\lib', title='Please select a watermark')
+    images_path = filedialog.askopenfilename(initialdir='D:\Programming\College Study\IG\waterMarking\lib',
+                                             title='Please select a watermark')
     return images_path
+
 
 def select_img_options():
     select_options = int(input("Choose : \n 1- browse image or multiple images \n 2- enter image path \n "))
@@ -77,14 +79,36 @@ def place_watermark(watermark_pos, watermark_img, original_img):
     return original_img
 
 
-if __name__ == '__main__':
-    images_path = select_img_options()
+def place_text_watermark(img, watermark_text, pos):
+    font = cv.FONT_HERSHEY_SIMPLEX
 
-    watermark_type = browse_image()
+    fontScale = 1
+    color = (255, 0, 0)
+    thickness = 2
+    cv.putText(img, watermark_text, pos, font, fontScale, color, thickness, cv.LINE_AA)
+
+
+def calcTextWatermarkPos(text, img):
+
+    font = cv.FONT_HERSHEY_SIMPLEX
+
+    textsize, _ = cv.getTextSize(text, font, 1, 2)
+    textX = (img.shape[1] - textsize[0]) / 2
+    textY = (img.shape[0] + textsize[1]) / 2
+    pos = (int(textX), int(textY))
+    return pos
+
+
+if __name__ == '__main__':
+
+    images_path = select_img_options()
+    watermark_type = select_watermark_options()
 
     for img_path in images_path:
+
         img = cv.imread(img_path)
         img = resize(50, img)
+        filename = os.path.basename(img_path)
 
         if os.path.exists(watermark_type):
             watermark_img = cv.imread(watermark_type)
@@ -93,11 +117,13 @@ if __name__ == '__main__':
             watermark_pos = calcCoordinates(img, watermark_img)
             image_after_watermark = place_watermark(watermark_pos, watermark_img, img)
 
-            filename = os.path.basename(img_path)
-            cv.imwrite("D:\Programming\College Study\IG\waterMarking\lib\watermarked_" + filename, img)
+            cv.imwrite('D:\Programming\College Study\IG\waterMarking\lib\watermarked_image_' + filename, img)
 
         else:
             watermark_text = watermark_type
-            print(watermark_text)
+
+            pos = calcTextWatermarkPos(watermark_text, img)
+            place_text_watermark(img, watermark_text, pos)
+            cv.imwrite('D:\Programming\College Study\IG\waterMarking\lib\watermarked_text_' + filename, img)
 
     cv.waitKey(0)
